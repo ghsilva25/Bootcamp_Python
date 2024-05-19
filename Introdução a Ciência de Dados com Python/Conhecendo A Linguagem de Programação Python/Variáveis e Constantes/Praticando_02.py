d73c3c4f0ab18220d5cdf4245351e2db7ea2a087
Gabriel Silva <114780718+ghsilva25@users.noreply.github.com>
2024-05-19 12:49:44 -0300

    Variáveis e Constantes

---
+++ Introdução a Ciência de Dados com Python/Conhecendo A Linguagem de Programação Python/Variáveis e Constantes/Praticando_02.py
@@ -0,0 +1,20 @@
+
+# Variáveis: PODE-SE DEFINIR VARIÁVEIS NA MESMA LINHA, ATRIBUINDO A VIRGULA PARA SEPARAR AS INFORMAÇÕES
+nome = "Gabriel Silva"
+idade = 22
+
+
+nome = "Giovanna"
+
+# Imprimir
+print(nome, idade)
+
+
+# utilizar este modelo para variáveis em python 
+limite_saque_diario = 1000
+
+
+BRAZILIAN_STATES = ["SP", "RJ", "SC", "RS"]
+
+print("BRAZILIAN_STATES")
+
