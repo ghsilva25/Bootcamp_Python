ceabafa9d871515322b70f756991f3b1a3a52635
Gabriel Silva <114780718+ghsilva25@users.noreply.github.com>
2024-05-19 13:53:03 -0300

    Operadores de Comparação

---
+++ Operadores de Comparação/Praticando_06.py
@@ -0,0 +1,14 @@
+#Atribuindo as variáveis:
+saldo = 200
+saque = 200
+
+#Imprimindo se é True ou False:
+print(saldo == saque) #igual
+print(saldo != saque) #diferente
+print(saldo > saque) #maior que
+print(saldo >= saque) #maior ou igual
+print(saldo < saque) #menor que
+print(saldo <= saque) #menor ou igual 
+
+
+
