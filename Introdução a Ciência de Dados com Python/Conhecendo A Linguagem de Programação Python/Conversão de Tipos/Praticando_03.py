3dcea776d8ba1f941e5e8a915b1625c570e19257
Gabriel Silva <114780718+ghsilva25@users.noreply.github.com>
2024-05-19 13:05:45 -0300

    Conversão de Tipos

---
+++ Introdução a Ciência de Dados com Python/Conhecendo A Linguagem de Programação Python/Conversão de Tipos/Praticando_03.py
@@ -0,0 +1,18 @@
+#Conversão de tipos numéricos.
+print(float(1))
+print(int(1.0))
+print(float(10.10))
+print(float(100))
+
+#Perguntando qual classe pertence.
+print(type(str(10.10)))
+
+
+valor = 10
+valor_str = str(valor)
+print(type(valor))
+print(type(valor_str))
+
+
+print(100/2) # cálculo com ponto flutuante.
+print(100 // 2) # cálculo sem ponto flutuante.
