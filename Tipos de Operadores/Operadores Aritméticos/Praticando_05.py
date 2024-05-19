c4a16c20c4864a6c55df3cfe400a5417594e9e9b
Gabriel Silva <114780718+ghsilva25@users.noreply.github.com>
2024-05-19 13:36:58 -0300

    Operadores Aritméticos

---
+++ Tipos de Operadores/Operadores Aritméticos/Praticando_05.py
@@ -0,0 +1,18 @@
+#Declarando variáveis:
+produto_1 = 10
+produto_2 = 20
+
+#Imprimindo as informações:
+print(produto_1 + produto_2) #somando
+print(produto_1 - produto_2) #subtraindo
+print(produto_1 // produto_2) #divisão decimal
+print(produto_1 / produto_2) #divisão inteira
+print(produto_1 * produto_2) #multiplicação
+print(produto_1 % produto_2) #resto da divisão
+print(produto_1 ** produto_2) #exponenciação
+
+
+# O Python entende a ordem dos fatores (Imporcedência numérica):
+x = 10 + 5 * 4
+print(x)
+
